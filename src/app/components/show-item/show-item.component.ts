00cd75fefc014deb5d26110ad0c73f1090462d99
Francesco-Rianna <maradonino98@hotmail.it>
2024-09-04 14:30:33 +0200

    printed in page the upcoming movies

--- src/app/components/show-item/show-item.component.ts
+++ src/app/components/show-item/show-item.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core'
+import { Movie } from '../../types/movie'
 
 @Component({
   selector: 'app-show-item',
   templateUrl: './show-item.component.html',
-  styleUrl: './show-item.component.scss'
+  styleUrl: './show-item.component.scss',
 })
 export class ShowItemComponent {
-
+  @Input() showItem: Movie | null = null
+  imgUrl = 'https://image.tmdb.org/t/p/w500/'
 }
